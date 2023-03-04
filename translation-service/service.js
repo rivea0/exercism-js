75fea1e59616b4fb1d779e44cce2451f8737135d
rivea0 <58330360+rivea0@users.noreply.github.com>
2023-03-04 18:49:39 +0300

    Add translation-service

---
+++ translation-service/service.js
@@ -0,0 +1,141 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+//
+// The lines above enable type checking for this file. Various IDEs interpret
+// the @ts-check and reference directives. Together, they give you helpful
+// autocompletion when implementing this exercise. You don't need to understand
+// them in order to use it.
+//
+// In your own projects, files, and code, you can play with @ts-check as well.
+
+export class TranslationService {
+    /**
+     * Creates a new service
+     * @param {ExternalApi} api the original api
+     */  
+    
+    constructor(api) {
+      this.api = api;
+    }
+  
+    /**
+     * Attempts to retrieve the translation for the given text.
+     *
+     * - Returns whichever translation can be retrieved, regardless the quality
+     * - Forwards any error from the translation api
+     *
+     * @param {string} text
+     * @returns {Promise<string>}
+     */
+    free(text) {
+      return this.api.fetch(text)
+        .then(value => value['translation'])
+        .catch(error => { throw error });
+    }
+  
+    /**
+     * Batch translates the given texts using the free service.
+     *
+     * - Resolves all the translations (in the same order), if they all succeed
+     * - Rejects with the first error that is encountered
+     * - Rejects with a BatchIsEmpty error if no texts are given
+     *
+     * @param {string[]} texts
+     * @returns {Promise<string[]>}
+     */
+    batch(texts) {
+      if (!texts.length) {
+        return Promise.reject(new BatchIsEmpty());
+      }
+      let translations = texts.map(text => this.free(text));
+      return Promise.all(translations);
+    }
+  
+    /**
+     * Requests the service for some text to be translated.
+     *
+     * Note: the request service is flaky, and it may take up to three times for
+     *       it to accept the request.
+     *
+     * @param {string} text
+     * @returns {Promise<void>}
+     */
+    request(text) {
+      let api = this.api;
+      function requestPromise(text) {
+        return new Promise((resolve, reject) => {
+          api.request(text, (error) => (error ? reject(error) : resolve()))
+        })
+      }
+      return requestPromise(text)
+        .catch(() => requestPromise(text))
+        .catch(() => requestPromise(text));
+    }
+  
+    /**
+     * Retrieves the translation for the given text
+     *
+     * - Rejects with an error if the quality can not be met
+     * - Requests a translation if the translation is not available, then retries
+     *
+     * @param {string} text
+     * @param {number} minimumQuality
+     * @returns {Promise<string>}
+     */
+    premium(text, minimumQuality) {
+      return this.api.fetch(text)
+        .then(value => {
+          if (value['quality'] >= minimumQuality) {
+            return Promise.resolve(value['translation']);
+          } else {
+            throw new QualityThresholdNotMet();
+          }
+        }, error => {
+            return this.request(text)
+              .then(_ => {
+                return this.api.fetch(text).then(t => {
+                  if (t['quality'] >= minimumQuality) {
+                    return Promise.resolve(t['translation']);
+                  } else {
+                    throw new QualityThresholdNotMet();
+                  }
+                });              
+              })
+              .catch(requestError => { throw requestError });
+          })
+    }
+  }
+  
+  /**
+   * This error is used to indicate a translation was found, but its quality does
+   * not meet a certain threshold. Do not change the name of this error.
+   */
+  export class QualityThresholdNotMet extends Error {
+    /**
+     * @param {string} text
+     */
+    constructor(text) {
+      super(
+        `
+  The translation of ${text} does not meet the requested quality threshold.
+      `.trim()
+      );
+  
+      this.text = text;
+    }
+  }
+  
+  /**
+   * This error is used to indicate the batch service was called without any
+   * texts to translate (it was empty). Do not change the name of this error.
+   */
+  export class BatchIsEmpty extends Error {
+    constructor() {
+      super(
+        `
+  Requested a batch translation, but there are no texts in the batch.
+      `.trim()
+      );
+    }
+  }
+  
